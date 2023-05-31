bee9f4653623c4da60602d0aa5d48ab682a5cec3
Abdulkabir Adedeji <kabir.adedeji@gmail.com>
2023-05-30 22:54:47 -0600

    implement head

---
+++ head.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const head = function (array) {
+  if (array.length !== 0) {
+  return array[0];
+  }
+};
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log("Assertion Passed: [actual] === [expected]");
+    } else {
+      console.log("Assertion Failed: [actual] !== [expected]");
+    }
+};
+
+
+
+// test cases
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 10);
+assertEqual(head([]), 5);
